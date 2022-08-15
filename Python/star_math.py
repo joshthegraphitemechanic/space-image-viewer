c1eeb44ea26519c420aafaaa47447468369a73bb
Joshua Glass <joshglas@mit.edu>
2022-08-16 01:18:33 +0200

    FIltering, RA needing range

--- Python/star_math.py
+++ Python/star_math.py
@@ -1,15 +1,32 @@
 from astroquery.mast import Observations
 import PyQt5
+from PyQt5 import QtCore
 
-test_query = {
-    "telescope": "Hubble",
-    "object name": "",
-    "object type": "N/A",
-    "galactic coordinates": ["", ""],
-    "date range": [PyQt5.QtCore.QDate(2000, 1, 1), PyQt5.QtCore.QDate(2000, 1, 1)],
-    "last_name": "",
-    "sort_by": "",
-}
+
+class PandasModel(QtCore.QAbstractTableModel):
+    """
+    Class to populate a table view with a pandas dataframe
+    """
+    def __init__(self, data, parent=None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return len(self._data.values)
+
+    def columnCount(self, parent=None):
+        return self._data.columns.size
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if index.isValid():
+            if role == QtCore.Qt.DisplayRole:
+                return str(self._data.iloc[index.row()][index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
 
 
 def populate_sources():
@@ -20,11 +37,26 @@ def populate_sources():
 def space_query(
     query_dict
 ):
-    telescope = query_dict["telescope"]
-    params = {"obs_collection": telescope}
-
+    params = {}
+    params["obs_collection"] = query_dict["telescope"]
+    if query_dict["object name"] != '':
+        params["target_name"] = query_dict["object name"]
+    if query_dict["object type"] != 'N/A':
+        params["target_classification"] = query_dict["object type"]
+    if query_dict["dec"] != '':
+        params["s_dec"] = query_dict["dec"]
+    if query_dict["ra"] != '':
+        params["s_ra"] = query_dict["ra"]
+    if query_dict["date toggle"]:
+        params["t_min"] = query_dict["date min"]
+        params["t_max"] = query_dict["date max"]
+    if query_dict["last name"] != '':
+        params["proposal_pi"] = query_dict["last name"]
+    sorting = query_dict["sort by"]
 
 
 
     obs_table = Observations.query_criteria(**params)
-    return(obs_table)
+    data = obs_table.to_pandas()
+    model = PandasModel(data)
+    return(model)
--- app.py
+++ app.py
@@ -3,35 +3,9 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import uic, QtCore
 from Python.star_math import *
 import pandas as pd
-
+from astropy.time import Time
 
 state_dict = {}
-
-
-class PandasModel(QtCore.QAbstractTableModel):
-    """
-    Class to populate a table view with a pandas dataframe
-    """
-    def __init__(self, data, parent=None):
-        QtCore.QAbstractTableModel.__init__(self, parent)
-        self._data = data
-
-    def rowCount(self, parent=None):
-        return len(self._data.values)
-
-    def columnCount(self, parent=None):
-        return self._data.columns.size
-
-    def data(self, index, role=QtCore.Qt.DisplayRole):
-        if index.isValid():
-            if role == QtCore.Qt.DisplayRole:
-                return str(self._data.iloc[index.row()][index.column()])
-        return None
-
-    def headerData(self, col, orientation, role):
-        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
-            return self._data.columns[col]
-        return None
 
 
 def write_state(win):
@@ -39,16 +13,14 @@ def write_state(win):
     state_dict["telescope"] = window.source_telescope_selector.currentText()
     state_dict["object name"] = window.object_name_edit.text()
     state_dict["object type"] = window.object_type_selector.currentText()
-    state_dict["galactic coordinates"] = [window.RA_edit.text(), window.DEC_edit.text()]
-    state_dict["date range"] = [
-        window.start_date_edit.date(),
-        window.end_date_edit.date(),
-    ]
-    state_dict["last_name"] = window.last_name_edit.text()
-    state_dict["sort_by"] = window.sort_by_selector.currentText()
-    display = space_query(state_dict).to_pandas()
-    model = PandasModel(display)
-    window.query_display.setModel(model)
+    state_dict["ra"] = window.RA_edit.text()
+    state_dict["dec"] = window.DEC_edit.text()
+    state_dict["date toggle"] = window.date_toggle.isChecked()
+    state_dict["date min"] = Time(window.start_date_edit.date().toString("yyyy-MM-dd"), scale='utc').mjd
+    state_dict["date max"] = Time(window.end_date_edit.date().toString("yyyy-MM-dd"), scale='utc').mjd
+    state_dict["last name"] = window.last_name_edit.text()
+    state_dict["sort by"] = window.sort_by_selector.currentText()
+    window.query_display.setModel(space_query(state_dict))
 
 
 
